15c559cc448ee689f65086b18c1d278cef3379cd
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-10 22:25:56 +0500

    changes logim

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -13,19 +13,23 @@ const userSchema = new Schema({
   password: {
     type: String,
     required: true
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  
-  
+  }
 })
 
 // static signup method
 userSchema.statics.signup = async function(email, password,name) {
 
-  
+  // validation
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
+
   const exists = await this.findOne({ email })
 
   if (exists) {
@@ -35,7 +39,27 @@ userSchema.statics.signup = async function(email, password,name) {
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password, salt)
 
-  const user = await this.create({ email, password: hash ,name})
+  const user = await this.create({ email, password: hash,name })
+
+  return user
+}
+
++// static login method
+      userSchema.statics.login = async function(email, password) {
+
+  if (!email || !password) {
+    throw Error('All fields must be filled')
+  }
+
+  const user = await this.findOne({ email })
+  if (!user) {
+    throw Error('Incorrect email')
+  }
+
+  const match = await bcrypt.compare(password, user.password)
+  if (!match) {
+    throw Error('Incorrect password')
+  }
 
   return user
 }
