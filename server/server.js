9103564c858d1752ae161b3ed7a6570d0a050e84
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-11 23:22:45 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/user')
-
+var cors = require("cors");
 // express app
 const app = express()
 
 // middleware
 app.use(express.json())
-
+app.use(cors());
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
