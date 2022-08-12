96b7275b0cba029d7b62721fde57cfbfa3f2b873
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-12 22:41:40 +0500

    Update tweetModel.js

--- server/models/tweetModel.js
+++ server/models/tweetModel.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
 const validator = require('validator')
 
 const Schema = mongoose.Schema
 
 const tweetSchema = new Schema({
   tweet: String,
-  likes:Number,
+  likes:{},
   user: {},
-  likedPersons:{}
+
 })
 
 // static signup method
@@ -33,27 +32,57 @@ tweetSchema.statics.getTweet = async function() {
 tweetSchema.statics.addTweetLike = async function(_id,email) {
 
   // validation
+
   if (!_id || !email) {
     throw Error('Failed to like Tweet')
   }
-
-  const tweet = await this.find({likedPersons:email})
-  
-      if(!tweet){
-     
-     const unlikedtweet =  await this.findById({ _id})
-      
-      if(unlikedtweet.likes){
-        unlikedtweet.likes += 1
-      }else{
-        unlikedtweet.likes = 1
+  const tweet  = await this.findById({ _id})
+      const data = {
+        email:email
       }
+      
+     if(tweet.likes){
      
-      unlikedtweet.save()
-      return unlikedtweet;
-    }
+      tweet.likes.forEach(email => {
+    
+       if(email.email == data.email){
 
-    throw Error('Already like the Tweet')
+        throw Error('Already like the tweet')
+       
+       }
+     
+      })
+    
+    let  utweet =  this.findOneAndUpdate(
+        { _id: _id }, 
+        { $push: { likes: data  } })
+    
+         return utweet;
+     }
+     else{
+  let  ftweet =  this.findOneAndUpdate(
+        { _id: _id }, 
+        { $push: { likes: data  } })
+        return ftweet;
+     }
+     
+  //  = await this.find({likedPersons.email:email})
+  
+    //   if(!tweet){
+     
+    //  const unlikedtweet = 
+      
+    //   if(unlikedtweet.likes){
+    //     unlikedtweet.likes += 1
+    //   }else{
+    //     unlikedtweet.likes = 1
+    //   }
+     
+    //   unlikedtweet.save()
+    //   return unlikedtweet;
+    // }
+
+    // throw Error('Already like the Tweet')
 
    
 }
