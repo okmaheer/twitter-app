d1645bab42af246b2533a4e35fd4ec2158481881
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-12 18:00:42 +0500

    Update Profile.vue

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="flex flex-col item-center justify-center h-screen">
     <div class=" max-w-2xl m-0 px-6 h-auto text-white">
-        <form class="bg-gray-700 shadow-md rounded px-8 pt-6 space-y-4 pb-8 mb-4">
-             <div class="flex items-center justify-end ">
-            <button
+        <form @submit.prevent="tweetHandler"  class="bg-gray-700 shadow-md rounded px-8 pt-6 space-y-4 pb-8 mb-4">
+          
+             <div class="flex items-center justify-between ">
+             <h1 class="text-lg">{{ authStore.getUser.name }}</h1>
+          <button
               class="
                 bg-blue-500
                 hover:bg-blue-700
@@ -14,15 +16,14 @@
                 rounded
                 focus:outline-none focus:shadow-outline
               "
-              type="button"
-            >
+              type="submit">
               Tweet
             </button>
     
           </div>
           <div class="mb-4">
           
-            <input
+            <textarea
               class="
                 shadow
                 appearance-none
@@ -35,7 +36,9 @@
                 leading-tight
                 focus:outline-none focus:shadow-outline
               "
+              v-model="tweet"
               id="tweet"
+              rows="5" cols="50"
               type="textarea"
               placeholder="What's happening?"
             />
@@ -50,12 +53,34 @@
 
 <script >
 // import TheLoginForm from '../components/TheLoginForm.vue'
+import { useAuthStore } from "../store";
+import { ref } from "vue";
+import { usetweetStore } from "../store/tweet";
+import { useRouter } from "vue-router";
 export default {
+
   components: {
     // TheLoginForm,
   },
-  setup() {
-    return {};
+  setup() {  
+       const router = useRouter();
+       const tweet = ref('')
+        const authStore = useAuthStore()
+        const tweetStore = usetweetStore()
+
+
+         const tweetHandler = async () => {
+      const res = await tweetStore.tweetAction(tweet.value);
+
+      if (res.type == "success") {
+        router.replace("/");
+      }
+         }
+    return {
+      authStore,
+      tweet,
+      tweetHandler
+    };
   },
 };
 </script>
