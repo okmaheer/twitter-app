0b22d0cc773f618d8890951f55785ed486a91b8e
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-12 18:00:07 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const userRoutes = require('./routes/user')
+const tweetRoutes = require('./routes/tweet')
 var cors = require("cors");
 // express app
 const app = express()
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/user', userRoutes)
+app.use('/api/tweet', tweetRoutes)
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
