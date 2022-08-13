49620097c008a41e2b401cdce40bc4c2bbf924bf
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-13 15:20:57 +0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 require('dotenv').config()
 
-const express = require('express')
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 const mongoose = require('mongoose')
+// Routes
 const userRoutes = require('./routes/user')
 const tweetRoutes = require('./routes/tweet')
 var cors = require("cors");
 // express app
-const app = express()
-
 // middleware
 app.use(express.json())
 app.use(cors());
@@ -24,7 +29,7 @@ app.use('/api/tweet', tweetRoutes)
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
-    app.listen(process.env.PORT, () => {
+    server.listen(process.env.PORT, () => {
       console.log('connected to db & listening on port', process.env.PORT)
     })
   })
