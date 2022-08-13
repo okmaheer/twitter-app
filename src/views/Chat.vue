e0d2727368fb14173993b8269d0d4a02c779a315
Maheer <86462580+okmaheer@users.noreply.github.com>
2022-08-13 15:21:11 +0500

    Create Chat.vue

---
+++ src/views/Chat.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+      
+  
+  </div>
+</template>
+
+<script>
+import { useChatStore } from "../store/chat";
+export default {
+   
+  setup () {
+    const chatStore = useChatStore()
+  
+  
+    return {
+      
+      chatStore,
+    }
+  }
+}
+</script>
